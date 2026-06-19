9ee45d9fadecafd5c2a856f8a26b5bed0be2d6a7
Justin <justintanjj192@gmail.com>
2026-06-19 12:31:32 +0800

    add invitation button that routes to blank invite page

---
+++ lib/pages/invitations/invitations_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InvitationsPage extends StatelessWidget {
+  const InvitationsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Invitations')),
+      body: const SizedBox.expand(),
+    );
+  }
+}
